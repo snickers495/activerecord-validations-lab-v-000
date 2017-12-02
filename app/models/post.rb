ad69c570ac4c22520947e46707c876afacfc79c6
Lisa Lee <snickers495@gmail.com>
2017-12-02 23:53:52 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  include ActiveModel::Validations
+  validates_with MyValidator
 end
