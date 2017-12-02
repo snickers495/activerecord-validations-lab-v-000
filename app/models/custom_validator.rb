7caf18165ddd1ddb412feda9018cc8febfa26b40
Lisa Lee <snickers495@gmail.com>
2017-12-02 23:55:08 +0000

    automatically backed up by learn

--- app/models/custom_validator.rb
+++ app/models/custom_validator.rb
@@ -1,8 +1,1 @@
-class MyValidator < ActiveModel::Validator
-  def validate(record)
-    clickbait = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
-     if clickbait.none? { |word| word.match(record.title) }
-       record.errors[:title] << 'Title needs to be clickbaity'
-     end
-  end
-end
+
