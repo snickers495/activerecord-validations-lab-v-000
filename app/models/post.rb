2a2edb44d47e394a6f1ca9efcdeccec79db1e819
Lisa Lee <snickers495@gmail.com>
2017-12-02 23:55:13 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    clickbait = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+     if clickbait.none? { |word| word.match(record.title) }
+       record.errors[:title] << 'Title needs to be clickbaity'
+     end
+  end
+end
+
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
   validates :title, presence: true
